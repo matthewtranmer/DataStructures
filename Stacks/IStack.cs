34e86e0be12362d06f9d8ea68e80b9fe74a07604
unknown <matthew.tranmer@outlook.com>
2022-03-05 21:38:08 +0000

    Added Stack Interface

---
+++ Stacks/IStack.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    interface IStack<T>
+    {
+        public bool IsFull { get; }
+        public bool IsEmpty { get; }
+
+        void Push(T item);
+        T Pop();
+    }
+}
